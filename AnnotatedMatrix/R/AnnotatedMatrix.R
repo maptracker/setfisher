005aeb2c615d313fbb335c2f3eb2a6d04ac7caa5
Charles Tilford <charles.tilford@bms.com>
2019-01-06 21:07:34 -0500

    WIP, commiting WFH code

--- AnnotatedMatrix/R/AnnotatedMatrix.R
+++ AnnotatedMatrix/R/AnnotatedMatrix.R
@@ -47,7 +47,19 @@ sfSep <- ' || ' # Token for separating text while recording filters
 #' framework. The package is also very well suited for identifier
 #' mapping (for example, gene symbols to gene accessions).
 #'
-#' @param file
+#' @param file Required (unless \code{obj} is specified), the file
+#'     path of the matrix to be loaded
+#' @param obj Default \code{NA}, alternative to \code{file}, builds a
+#'     matrix from an existing R object
+#' @param params Default \code{NA}. Optional list of parameters to set
+#'     at load. Parameters can also be set later using either
+#'     \link{param} or \link{setParamList}.
+#' @param autofilter Default \code{TRUE}, which will cause any default
+#'     filters specified by the matrix file to be applied. Set to
+#'     \code{FALSE} to suppress automated filtering on loading
+#' @param help Default \code{FALSE}, if true show this help and do not
+#'     create a new object
+#' @param \dots, passed to \link{.buildMatrix} and \link{.readMatrix}
 #'
 #' @field file Path to file the matrix was loaded from
 #' @field fromRDS Logical, true if the loaded file was an RDS object
@@ -111,6 +123,14 @@ sfSep <- ' || ' # Token for separating text while recording filters
 #' 
 #' @export AnnotatedMatrix
 #' @exportClass AnnotatedMatrix
+#'
+#' @examples
+#'
+#' demoFile <- annotatedMatrixExampleFile() # Small demo matrix
+#' am <- AnnotatedMatrix( demoFile )
+#' am$help() # Broad summary of available functionality
+#' am$map( help=TRUE ) # Help on a specific object method
+#' am$showParameters( na.rm=FALSE ) # Show all available parameters
 #' 
 
 AnnotatedMatrix <-
@@ -181,7 +201,7 @@ AutoFilterComment [character] Optional message displayed when automatic filters 
 ", ...)
         fromRDS  <<- FALSE
         modState <<- 0
-        if CatMisc::is.def(obj)) {
+        if (CatMisc::is.def(obj)) {
             file      <<- as.character(NA)
             .readObject( obj )
         } else {
@@ -2445,16 +2465,19 @@ ToDo: STILL WORKING ON ROUND-TRIP PARSING FILTER TEXT
         if (help) return( CatMisc::methodHelp(match.call(), class(.self),
                                               names(.refClassDef@contains)) )
         sm <- storaage.mode(obj)
-        if (sm = 'list') {
-            dateMessage(paste("Reading simple list from file", colName))
-            rv <- parse_Text_file( file=file, ... )
+        if (sm == 'list') {
+            dateMessage("Parsing list object into matrix")
+            ## We will take the list names as one dimension, and the
+            ## list contents as the other
+            
+            stop("WORK IN PROGRESS - list() IMPORT NOT YET IMPLEMENTED")
 
         } else {
             err(c("Attempt to create an AnnotatedMatrix file with ", sm,
                   " object - No logic defined to load such objects"),
                 fatal = TRUE)
         }
-        
+        rv
     },
 
     metadata_keys = function ( help=FALSE ) {
@@ -2567,13 +2590,51 @@ ToDo: STILL WORKING ON ROUND-TRIP PARSING FILTER TEXT
         rv
     },
 
-    .fieldDescriptions = function ( update=TRUE, help=FALSE ) {
-        "Update object fields to include attributes with brief descriptions"
-        if (help) return( CatMisc::methodHelp(match.call(), class(.self),
-                                              names(.refClassDef@contains)) )
-        fields <- list(
+    helpSections = function ( help=FALSE ) {
+        "Static list organizing object methods into conceptual sections"
+        if (help) return( CatMisc::methodHelp(match.call(), class(.self) ) )
+        list(
+            "Primary Operation" = c(
+                "map", "product", "matObj",
+                "as.gmt", "as.ijx", "as.mtx", "as.sidecar", "melt"),
+            "Filtering the Matrix" = c(
+                "filterByScore", "filterByCount", "filterByFactorLevel",
+                "filterById", "filterByMetadata",
+                "rNames", "cNames",
+                "removeEmptyRows", "removeEmptyCols", "removeEmpty",
+                "reset", "autoFilter", "filterSummary", "appliedFilters"),
+            "Matrix Information" = c(
+                "rCounts", "cCounts","populatedRows",
+                "populatedCols", "nnZero",
+                "is.factor", "levels", "sharedDimensions"),
+            "Metadata" = c("metadata", "metadata_keys"),
+            "Parameter Management" = c(
+                "param", "showParameters", "allParams", "defineParameters",
+                "paramClass", "paramDefinition", "paramName", "setParamList",
+                "hasParam" ),
+            "Event Logging" = c(
+                "message","showLog","verbose","useColor" ),
+            "Utility Methods" = c(
+                "matrixText"),
+            "Internal Methods" = c(
+                ".addAppliedFilter", ".autoLevel",
+                ".readMatrix", ".readObject", ".buildMatrix",
+                ".detailZeroedRowCol", ".filterDetails" ),
+            "SKIP" = c(
+                "actionMessage","dateMessage","debugMessage","err",
+                "colNameToFunc","colorMap","colorize", ".addParamKey",
+                "initialize","tidyTime", "help", "annotateFields",
+                "fieldDescriptions", "getFieldDescriptions",
+                "helpSections", "getHelpSections")
+        )
+    },
+
+    fieldDescriptions = function(help=FALSE) {
+        "A static list of brief descriptions for each field in this object"
+        if (help) return( CatMisc::methodHelp(match.call(), class(.self) ) )
+        list(
             file       = "Path to the matrix source file",
-            fromRDS    = "Logical flag indicating file was read from cached RDS",
+            fromRDS    = "Logical flag if file was read from cached RDS",
             matrixRaw  = "Un-filtered sparse matrix as loaded from file",
             matrixUse  = "Sparse matrix after filtering (null if no filters)",
             matrixMD   = "Metadata data.table, both rows and columns",
@@ -2584,16 +2645,6 @@ ToDo: STILL WORKING ON ROUND-TRIP PARSING FILTER TEXT
             colChanges = "Named vector of any col names that needed alteration",
             colDefs    = "List of metadata column definitions"
         )
-        if (update) {
-            hfmt <- " help('%s', 'AnnotatedMatrix') # More information on field "
-            for (fld in names(fields)) {
-                if (is.null(.self[[fld]])) next # Can't attribute NULL
-                ## The [[ accessor seems to work for fields?
-                attr(.self[[fld]], "Description") <- fields[[fld]]
-                attr(.self[[fld]], "Help") <- sprintf(hfmt,fld)
-            }
-        }
-        fields
     },
 
     show = function (...) { cat( .self$matrixText(...) ) },
@@ -2751,109 +2802,6 @@ ToDo: STILL WORKING ON ROUND-TRIP PARSING FILTER TEXT
                     "\n"), collapse = "\n")
         ## Ending up with extraneous newlines at end
         sub("\n+$", "\n", msg)
-    },
-
-    help = function (color=NULL, help=FALSE) {
-        "Display high-level help about all object methods"
-        if (help) return( CatMisc::methodHelp(match.call(), class(.self),
-                                              names(.refClassDef@contains)) )
-        if (is.null(color)) color <- useColor() # Use EventLogger setting
-        doCol   <- if (color) { .self$colorize } else { function(x, ...) x }
-        objName <- .self$.selfVarName("myMatrix")
-        whtName <- doCol(objName, "white")
-        comCol  <- "yellow"
-
-        ## Figure out all available methods:
-        allMeth <- AnnotatedMatrix$methods()
-        ## Subtract out the generic ones:
-        allMeth <- setdiff(allMeth, c("callSuper", "copy", "export", "field", "getClass", "getRefClass", "import", "initFields", ".objectPackage", ".objectParent", "show", "trace", "untrace", "usingMethods"))
-        ## Subtract out some superclasses
-        allMeth <- allMeth[ !grepl('#', allMeth) ]
-
-        ## Organization of primary methods:
-        sections <- list(
-            "Primary Operation" = c("map", "product", "matObj", "as.gmt", "as.ijx", "as.mtx", "as.sidecar", "melt"),
-            "Filtering the Matrix" = c("filterByScore", "filterByCount", "filterByFactorLevel", "filterById", "filterByMetadata", "rNames", "cNames", "removeEmptyRows", "removeEmptyCols", "removeEmpty", "reset", "autoFilter", "filterSummary", "appliedFilters"),
-            "Matrix Information" = c("rCounts", "cCounts","populatedRows", "populatedCols", "nnZero", "is.factor", "levels"),
-            "Metadata" = c("metadata", "metadata_keys"),
-            "Parameter Management" = c("param", "showParameters", "allParams", "defineParameters","paramClass", "paramDefinition", "paramName", "setParamList", "hasParam" ),
-            "Event Logging" = c("message","showLog","verbose","useColor" ),
-            "SKIP" = c("actionMessage","dateMessage","debugMessage","err",
-                       "colNameToFunc","colorMap","colorize",
-                       "initialize","tidyTime"),
-            "Internal Methods" = allMeth[ grepl('^\\.', allMeth) ])
-        ## Everything else:
-        sections[["Other Methods"]] <- setdiff(allMeth, unname(unlist(sections)))
-
-        ## Basic header:
-        txt <- sprintf("
-%s
-?AnnotatedMatrix %s
-%s                 %s
-%s$showLog()       %s
-str(%s, max.lev=3) %s
-",
-doCol("###
-### Annotated Matrix Help - call the below commands for more details
-###","magenta"),
-doCol("# Built-in documentation on the class", comCol),
-whtName, doCol("# Summary report of the object", comCol),
-whtName, doCol("# Show logged events with timing", comCol),
-whtName, doCol("# Inspect the object structure", comCol))
-
-        noHelp <- c()
-        ## Add snippets for each method, broken down by section
-        for (sec in names(sections)) {
-            if (sec == "SKIP") next
-            txt <- c(txt, doCol(paste("\n############\n###", sec, "\n"),comCol))
-            meths <- sections[[ sec ]]
-            for (meth in meths) {
-                ## Going to see if we can extract the ROxygen
-                ## description string from the method
-                code <- utils::capture.output(AnnotatedMatrix$methods(meth))
-                ## Should not happen, but be safe:
-                if (is.null(code)) next
-                isHelped <- FALSE
-                com <- NA
-                for (line in code) {
-                    ## See if 'help=FALSE' is set - indicates I have
-                    ## tied it into my internalized help framework:
-                    if (grepl('help\\s*=\\s*FALSE', line))  isHelped <- TRUE
-                    cm <- CatMisc::parenRegExp('^\\s+"(.+)"\\s*$', line)
-                    if (!is.na(cm[1])) {
-                        ## Found a single line quoted string, presume
-                        ## it is description and stop scanning code:
-                        com <- cm[1]
-                        break
-                    }
-                }
-                if (!isHelped) {
-                    noHelp <- c(noHelp, meth)
-                    next
-                }
-                txt <- c(txt, sprintf("%s$%s( help=TRUE )", whtName, meth))
-                if (!is.na(com)) txt <-
-                     c(txt, doCol(paste("\n    #", com), comCol))
-                txt <- c(txt, "\n")
-            }
-        }
-        noHelp <- setdiff(noHelp, 'help')
-        if (length(noHelp) > 0) txt <- c(txt,
-              doCol("\n### Methods lacking help\n", comCol),
-              sprintf("# %s\n", strwrap(paste(noHelp, collapse=' '))))
-
-        txt <- c(txt, doCol("\n### Object fields\n", comCol))
-        ## Some fields are expected to be small structures, don't need str():
-        simpleFields <- unlist(strsplit("file fromRDS filterLog setFilters lvlVal colDefs", " ")) # Just a lazy way to manage the list
-        strFmt <- "str(%s$%s) %s\n"
-        simFmt <- "%s$%s %s\n"
-        fields <- .fieldDescriptions( )
-        for (field in names(fields)) {
-            fmt <- ifelse(is.element(field, simpleFields), simFmt, strFmt)
-            txt <- c(txt, sprintf(fmt, whtName, field,
-                                  doCol(paste("#",fields[[field]]), comCol)))
-        }
-        base::message(paste(txt, collapse='', sep=''))
-        invisible(NULL)
     }
+
 )
